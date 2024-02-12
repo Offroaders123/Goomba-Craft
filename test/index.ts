82bde29f2c4e6ef09b0075dc7ff1445e689b262a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-02-11 21:10:44 -0800

    Test-Relative URL Scoping
    Also realized I forgot that it's necessary to call the cleanup function to close the DB as well, after having opened it.

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,11 @@
+import { fileURLToPath } from "node:url";
 import { readable } from "../node-cdb/dist/index.js";
 
-const db = new readable("./world/9gAAAAsyAwA/db/cdb/index.cdb");
+const world: string = fileURLToPath(new URL("./world/9gAAAAsyAwA/db/cdb/index.cdb",import.meta.url));
+
+const db = new readable(world);
 await db.openAsync();
 
 console.log(db);
+
+await db.closeAsync();

8fe5e83428594282f9bdd329c6f65e5e08eeccbb
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-09-01 11:46:14 -0700

    VDB Temp Detection
    They follow a specific format, I am just kind of forcing the correct parsing of it in this edit.
    
    Going to look into this one next, for CDB files
    https://minecraft.wiki/w/Bedrock_Edition_level_format/History#SubChunkPrefix_record_(1.0_and_1.2.13_formats)
    
    Manual Manic Procedures
    Midnight Priest - Aggressive Hauntings

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { read, type NBTData } from "nbtify";
 
 export async function readVDB(data: Uint8Array): Promise<NBTData> {
-  return await read(data.subarray(0x35), {
-    rootName: true,
-    endian: "little",
-    compression: null,
-    bedrockLevel: false,
-    strict: false
-  });
+  let byteOffset: number = 0x23;
+  while (true) {
+    try {
+      return await read(data.subarray(byteOffset++), {
+        rootName: true,
+        endian: "little",
+        compression: null,
+        bedrockLevel: false,
+        strict: false
+      });
+    } catch {}
+  }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,16 @@
-import { readFile } from "node:fs/promises";
+import { readdir, readFile } from "node:fs/promises";
+import { join } from "node:path";
+import type { NBTData } from "nbtify";
 import { readVDB } from "../src/index.js";
 
-const vdb = await readFile(new URL("./world/9gAAAAsyAwA/db/vdb/slt18.vdb", import.meta.url));
-console.log(vdb);
+const vdbs: [string, Buffer][] = await Promise.all(
+  (await readdir(new URL("./world/9gAAAAsyAwA/db/vdb", import.meta.url), { withFileTypes: true }))
+    .filter(entry => entry.name.includes("slt"))
+    .map(async entry => [entry.name, await readFile(join(entry.parentPath, entry.name))])
+);
+console.log(vdbs.map(([ name ]) => name));
 
-const player = await readVDB(vdb);
-console.log(player);
+const vnbts: [string, NBTData][] = await Promise.all(
+  vdbs.map(async ([ name, data ]) => [name, await readVDB(data)])
+);
+console.log(vnbts);

3786dc07b3e564450ce6b714deb2c13e2e8ed72c
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-09-01 12:05:36 -0700

    Read VDBS Test Abstraction
    Moving this off of the main scope so I can start working on CDB files too.

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,16 @@
 import { readdir, readFile } from "node:fs/promises";
 import { join } from "node:path";
+import { fileURLToPath } from "node:url";
 import type { RootTag } from "nbtify";
 import { readVDB } from "../src/index.js";
 
+const world: string = fileURLToPath(new URL("./world/9gAAAAsyAwA", import.meta.url));
+
+await readVDBS(world);
+
+async function readVDBS(path: string): Promise<void> {
 const vdbs: [string, Buffer][] = await Promise.all(
-  (await readdir(new URL("./world/9gAAAAsyAwA/db/vdb", import.meta.url), { withFileTypes: true }))
+  (await readdir(join(path, "db/vdb"), { withFileTypes: true }))
     .filter(entry => entry.name.includes("slt"))
     .map(async entry => [entry.name, await readFile(join(entry.parentPath, entry.name))])
 );
@@ -16,4 +22,5 @@ const vnbts: [string, RootTag][] = await Promise.all(
 
 for (const vnbt of vnbts) {
   console.log(...vnbt, "\n");
+}
 }
